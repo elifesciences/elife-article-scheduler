1679c80540cfa0d68b3c1b47966d5601db0a640a
Luke Skibinski <l.skibinski@elifesciences.org>
2017-10-30 15:09:40 +1030

    manage.sh now accepts a --skip-install command - this is important if it's called every minute by a cron job install.sh migrate command no longer prompts for input

--- manage.sh
+++ manage.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 # @description convenience wrapper around Django's runserver command
-source install.sh > /dev/null
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+skip_install=$1
+if test "$skip_install" = "--skip-install"; then
+    shift # like 'pop' but for positional args
+else
+    source install.sh > /dev/null
+fi
+
+source venv/bin/activate
 ./src/manage.py $@
