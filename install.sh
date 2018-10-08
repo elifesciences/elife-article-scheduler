ac07649eefc46fb5a61dc583da95390f155436d8
Luke <lsh-0@users.noreply.github.com>
2018-10-08 14:54:25 +1030

    dep bump. held django-rest-framework back. typo in lint script

--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e # everything must succeed.
+echo "[-] install.sh"
 
-python=/usr/bin/python3.5
-py=${python##*/} # ll: python3.5
-
-# check for exact version of python3
-if [ ! -e "venv/bin/$py" ]; then
-    echo "could not find venv/bin/$py, recreating venv"
-    rm -rf venv
-    $python -m venv venv
-fi
+. mkvenv.sh
 
 source venv/bin/activate
 
@@ -23,3 +16,5 @@ fi
 pip install -r requirements.txt
 
 python src/manage.py migrate --no-input
+
+echo "[✓] install.sh"
